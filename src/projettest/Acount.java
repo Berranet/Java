00a3162c4a37aa36fc55541cfb7e859616a0cf6c
berranet <berranet@yahoo.ca>
2011-10-19 15:08:54 -0400

    add differente method to the program

---
+++ src/projettest/Acount.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projettest;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ *
+ * @author Berra
+ */
+public class Acount {
+   int count = 0;
+   
+   void increase(){
+   
+       count ++;
+   }
+    void check()
+    {
+        throw new NotImplementedException();
+    }
+}
